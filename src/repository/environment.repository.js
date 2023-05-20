c3ac67bbe91199f52eaa4f0d5e3c8df6ea17ba92
Stanislav Minko <minkostas@gmail.com>
2023-05-20 12:18:15 +0300

    environment: updateById

--- src/repository/environment.repository.js
+++ src/repository/environment.repository.js
@@ -1,34 +1,45 @@
 const pool = require("../db");
 
-async function getAllDataDB(){
-    const client = await pool.connect();
-    const sql = `SELECT * FROM environment`
-    const result = (await client.query(sql)).rows;
-    return result;
+async function getAllDataDB() {
+  const client = await pool.connect();
+  const sql = `SELECT * FROM environment`;
+  const result = (await client.query(sql)).rows;
+  return result;
 }
 
-async function getDataByIdDB(id){
-    const client = await pool.connect();
-    const sql = `SELECT * FROM environment WHERE id = $1`;
-    const result = (await client.query(sql, [id])).rows;
-    return result;
+async function getDataByIdDB(id) {
+  const client = await pool.connect();
+  const sql = `SELECT * FROM environment WHERE id = $1`;
+  const result = (await client.query(sql, [id])).rows;
+  return result;
 }
 
-async function createDataDB(label, category, priotity){
-    const client = await pool.connect();
-    const sql =
-    `INSERT INTO environment(label, category, priotity) values ($1, $2, $3) returning *`;
-    const result = (await client.query(sql, [label, category, priotity])).rows;
-     return result;
+async function createDataDB(label, category, priotity) {
+  const client = await pool.connect();
+  const sql = `INSERT INTO environment(label, category, priotity) values ($1, $2, $3) returning *`;
+  const result = (await client.query(sql, [label, category, priotity])).rows;
+  return result;
 }
-async function deleteDataDB(id){
-    const client = await pool.connect();
-    const sql =
-    `DELETE FROM environment WHERE id = $1`;
-    const result = (await client.query(sql, [id])).rows;
-    const sql1 = `SELECT * FROM environment`;
+async function updateDataDB(id, label, category, priotity) {
+  const client = await pool.connect();
+  const sql = `UPDATE environment SET label=$1, category=$2, priotity=$3 WHERE id = $4 returning *`;
+  const result = (await client.query(sql, [label, category, priotity, id])).rows;
+  return result;
+}
 
-    const rez = (await client.query(sql1)).rows;
-    return rez;
+async function deleteDataDB(id) {
+  const client = await pool.connect();
+  const sql = `DELETE FROM environment WHERE id = $1`;
+  const result = (await client.query(sql, [id])).rows;
+  const sql1 = `SELECT * FROM environment`;
+
+  const rez = (await client.query(sql1)).rows;
+  return rez;
 }
-module.exports = {getAllDataDB, getDataByIdDB, createDataDB, deleteDataDB}
+module.exports = {
+  getAllDataDB,
+  getDataByIdDB,
+  createDataDB,
+  deleteDataDB,
+  updateDataDB,
+};
--- src/service/environment.service.js
+++ src/service/environment.service.js
@@ -1,26 +1,32 @@
 const {
-    getAllDataDB,
-    getDataByIdDB,
-    createDataDB,
-    deleteDataDB,
-    } = require("../repository/environment.repository");
+  getAllDataDB,
+  getDataByIdDB,
+  createDataDB,
+  deleteDataDB,
+  updateDataDB,
+} = require("../repository/environment.repository");
 
-async function getAllData(){
-    const data = await getAllDataDB();
-    return data;
+async function getAllData() {
+  const data = await getAllDataDB();
+  return data;
 }
-async function getDataById(id){
-    const data = await getDataByIdDB(id);
-    return data;
-}
-
-async function createData(label, category, priotity){
-    const data = await createDataDB(label, category, priotity);
-    return data;
+async function getDataById(id) {
+  const data = await getDataByIdDB(id);
+  return data;
 }
 
-async function deleteDataById(id){
-    const data = await deleteDataDB(id);
-    return data;
+async function createData(label, category, priotity) {
+  const data = await createDataDB(label, category, priotity);
+  return data;
 }
-module.exports = {getAllData, getDataById, createData, deleteDataById}
+
+async function updateDataById(id, label, category, priotity) {
+  const data = await updateDataDB(id, label, category, priotity);
+  return data;
+}
+
+async function deleteDataById(id) {
+  const data = await deleteDataDB(id);
+  return data;
+}
+module.exports = { getAllData, getDataById, createData, deleteDataById, updateDataById };
